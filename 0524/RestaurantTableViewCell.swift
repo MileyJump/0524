c43b3525d4871116a840ebdbe72babc452cb6002
choiminkyung <choialsrud2002@gmail.com>
2024-05-26 19:44:18 +0900

    식당 정보 프로젝트 1. 테이블뷰로 구현 2. Kingfisher 라이브러리 사용 3. 좋아요 버튼 구현

---
+++ 0524/RestaurantTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  RestaurantTableViewCell.swift
+//  0524
+//
+//  Created by 최민경 on 5/26/24.
+//
+
+import UIKit
+
+class RestaurantTableViewCell: UITableViewCell {
+
+ 
+    @IBOutlet var restaurantImageView: UIImageView!
+    
+    @IBOutlet var typeLabel: UILabel!
+    @IBOutlet var longitudeLabel: UILabel!
+    @IBOutlet var latitudeLabel: UILabel!
+    @IBOutlet var phoneNumberLabel: UILabel!
+    @IBOutlet var addressLabel: UILabel!
+    @IBOutlet var nameLabel: UILabel!
+    @IBOutlet var priceLabel: UILabel!
+    @IBOutlet var categoryLabel: UILabel!
+    @IBOutlet var heartButton: UIButton!
+}
