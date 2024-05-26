8d23c8d726971b5dcda1ea529f3fafea4bb23705
choiminkyung <choialsrud2002@gmail.com>
2024-05-26 13:53:42 +0900

    여행 관련 미니 프로젝트 1. 셀 UIImage, title, subtitle, date 구성 2. Kingfisher 라이브러리 활용 ->  네트워킹을 통해 웹의 이미지 로드를 쉽게 불러옴 3. date를 yy년 MM월 dd일 형태로 변환 -> String yyMMdd 형태를 을 OutPut   yy년 MM월 dd일 형태로 변환

---
+++ 0524/TravelTableViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  TravelTableViewCell.swift
+//  0524
+//
+//  Created by 최민경 on 5/26/24.
+//
+
+import UIKit
+
+class TravelTableViewCell: UITableViewCell {
+ 
+    @IBOutlet var travelImageView: UIImageView?
+    @IBOutlet var titleLabel: UILabel!
+    @IBOutlet var subTitleLabel: UILabel!
+    @IBOutlet var dateLabel: UILabel!
+    
+}
