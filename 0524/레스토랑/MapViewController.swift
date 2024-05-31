2d9f6d4901b0dc887c608531d57bfc96e919879c
choiminkyung <choialsrud2002@gmail.com>
2024-06-01 00:24:20 +0900

    레스토랑 화면 - 지도 어노테이션 1. Alert 타이틀에 맞는 위도, 경도 지도에 표시 2. 반경 500미터 설정

--- 0524/레스토랑/MapViewController.swift
+++ 0524/레스토랑/MapViewController.swift
@@ -20,27 +20,34 @@ class MapViewController: UIViewController {
         
         let right = UIBarButtonItem(title: "Filter", style: .plain, target: self, action: #selector(filterButtonTapped))
         navigationItem.rightBarButtonItem = right
-        
+
+//        mapViewConfigure()
     }
-    var latitude: [Double] = []
-    var longitude: [Double] = []
+    var latitudeList: [Double] = []
+    var longitudeList: [Double] = []
+    var restaurantname: [String] = []
     
     func filterRestaurant(for category: String) {
-        latitude.removeAll()
-        longitude.removeAll()
+        latitudeList.removeAll()
+        longitudeList.removeAll()
+        restaurantname.removeAll()
         
         for restaurant in restaurantList {
             if category == "전체" || restaurant.category == category {
-                latitude.append(restaurant.latitude)
-                longitude.append(restaurant.longitude)
+                latitudeList.append(restaurant.latitude)
+                longitudeList.append(restaurant.longitude)
+                restaurantname.append(restaurant.name)
+                
             }
         }
-        print(latitude, longitude)
+        print(latitudeList, longitudeList)
+        print(restaurantname)
         // 여기에 latitudes와 longitudes를 사용하여 지도 업데이트 코드를 추가할 수 있습니다.
+        mapViewConfigure()
+        
     }
     
     @objc func filterButtonTapped() {
-        print("Dd")
         let categories = ["전체", "한식", "중식", "카페", "분식", "일식", "경양식", "양식"]
         let handlers: [(UIAlertAction) -> Void] = [
             { _ in self.filterRestaurant(for: "전체")},
@@ -63,7 +70,18 @@ class MapViewController: UIViewController {
     }
     
     func mapViewConfigure() {
-        
+        for (latitude, longitude) in zip(latitudeList, longitudeList) {
+            let center = CLLocationCoordinate2D(latitude: latitude, longitude: longitude)
+            mapView.region = MKCoordinateRegion(center: center, latitudinalMeters: 500, longitudinalMeters: 500)
+            let annotation = MKPointAnnotation()
+            annotation.coordinate = center
+//            annotation.title = restaurantname
+            mapView.addAnnotation(annotation)
+            print("잘 되고 있군")
+//            mapView.delegate = self
+        }
+       
+        print("굿")
         
     }
     
